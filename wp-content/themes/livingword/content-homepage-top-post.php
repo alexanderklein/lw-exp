fbcb260a13718f77721241561bb10de77ca2e3df
Alexander Klein <alexander.klein@de.ignitiongroupe.com>
2018-09-20 17:14:49 +0200

    Baner created, Blog section moved to top and adjusted to updated layout.

---
+++ wp-content/themes/livingword/content-homepage-top-post.php
@@ -0,0 +1,32 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <a class="post-item<?php echo ($has_thumbnail && $post_index == 0) ? ' has-thumbnail' : ''; ?><?php echo ($post_index > 0) ? ' small' : ''; ?> " href="<?php the_permalink(); ?>" data-animation='{"animation": "slide", "delay": <?php echo 1 + $post_index * 0.15; ?>, "group": 4}'>
+    <!-- scroll-anim fade slide-anim -->
+        <div class="wrapper clearfix top-posts">
+            <div class="post-infos">
+                <?php if($has_thumbnail && $post_index == 0) {?>
+                <p class="posts-headline">Our latest Journal posts.</p>
+                <?php } ?>
+                <span class="post-category small"><span><?php echo $cat_name; ?></span></span>
+                <span class="post-date small"><?php echo get_the_date('M j, Y', $post_id); ?></span>
+            </div>
+            <div class="post-content">
+                <div class="post-title subtitle"><?php the_title(); ?></div>
+
+                <?php if($post_index == 0): ?>
+                    <?php if($excerpt != ''): ?>
+                    <!-- <div class="post-excerpt copy"><?php echo get_ellipsis_content(get_the_excerpt(), 380); ?></div> -->
+                    <div class="post-excerpt copy"><?php echo get_front_page_excerpt(300, get_the_excerpt()); ?></div>
+                    <?php endif; ?>
+                <?php endif; ?>
+            </div>
+
+            <?php if($post_index == 0): ?>
+                <?php if(has_post_thumbnail($post_id)): ?>
+                <div class="post-thumbnail">
+                    <?php echo get_the_post_thumbnail($post_id, 'full'); ?>
+                </div>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </a>
+</article>
--- wp-content/themes/livingword/front-page.php
+++ wp-content/themes/livingword/front-page.php
@@ -13,36 +13,90 @@ if (count($homepage_translation_items) > 2) {
 }
 ?>
     <?php while (have_posts()): the_post();?>
-	    <div id="stage-wrapper">
-	        <div id="stage">
-	        <h1>Lorem Ipsum</h1>
-	        </div>
-	        </div>
-								<!--
-								    <div id="first-vertical-line" class="vertical-line scroll-fade"></div>
-								    -->
+		    <div id="stage-wrapper" style="background-image: url(http://localhost:8888/wp-content/uploads/2018/01/Yellow-Header.jpg);">
+		        <div id="stage" style="background-image: url(http://localhost:8888/wp-content/uploads/2018/01/Yellow-Header.jpg);">
+	            <h1>Translation as an artform<span class="white">.</span></h1>
+	            <div id="sub-headline-wrapper">
+	            <p>From straightforward translation to creative adaptations we add passion, bringing your message to live.</p>
+    </div>
+    <a class="homepage-cta" href="/contact">Get in touch.</a>
+                </div>
+		        </div>
+		        </div>
+    <section class="journal-section">
+        <div class="section">
+            <div class="title-line scroll-anim expand-anim fade" data-animation='{"animation": "expand", "delay": 0.5, "group": 4}'></div>
 
-								    <section id="our-services" class="our-services-section">
-								        <div class="section">
-								            <h3 class="title scroll-anim type-anim ui" data-animation='{"animation": "type", "group": 1}'>
-								                <?php echo wrap_letters(get_field('our_services_title')); ?>
-								            </h3>
-								            <div class="title-line scroll-anim expand-anim fade" data-animation='{"animation": "expand", "delay": 0.5, "group": 1}'></div>
+            <?php
+$args = array(
+    'post_type' => array('post'),
+    'post_status' => array('publish'),
+    'posts_per_page' => '3',
+    'order_by' => 'date',
+    'order' => 'DESC',
+);
 
-								            <div id="services-container" class="wrapper ui scroll-fade white-bg">
-								                <?php
+// The Query
+$loop = new WP_Query($args);
+?>
+            <div id="posts-list" class="ui white-bg">
+                <?php
+$post_index = 0;
+while ($loop->have_posts()): $loop->the_post();
+
+    $post_id = get_the_ID();
+
+    $categories = get_the_category();
+    $cat_name = $categories[0]->cat_name;
+    $excerpt = get_the_excerpt();
+
+    $has_thumbnail = has_post_thumbnail($post_id);
+    ?>
+									                    <?php
+    set_query_var('post_index', $post_index);
+    set_query_var('post_id', $post_id);
+    set_query_var('has_thumbnail', $has_thumbnail);
+    set_query_var('cat_name', $cat_name);
+    set_query_var('excerpt', $excerpt);
+
+    get_template_part('content', 'homepage-top-post');
+
+    $post_index++;
+    ?>
+
+									                <?php endwhile;?>
+                <?php wp_reset_postdata();?>
+            </div>
+            <div class="wrapper right-align">
+                <a id="more-articles-link" class="strong" href="<?php echo get_permalink(get_page_by_path('journal')); ?>" data-animation='{"animation": "slide", "delay": 1.45, "group": 4}'>View more articles.</a>
+            </div>
+        </div>
+    </section>
+									<!--
+									    <div id="first-vertical-line" class="vertical-line scroll-fade"></div>
+									    -->
+
+									    <section id="our-services" class="our-services-section">
+									        <div class="section">
+									            <h3 class="title scroll-anim type-anim ui" data-animation='{"animation": "type", "group": 1}'>
+									                <?php echo wrap_letters(get_field('our_services_title')); ?>
+									            </h3>
+									            <div class="title-line scroll-anim expand-anim fade" data-animation='{"animation": "expand", "delay": 0.5, "group": 1}'></div>
+
+									            <div id="services-container" class="wrapper ui scroll-fade white-bg">
+									                <?php
     $services = get_field('services_list');
     $services_list = array();
 
     if (!empty($services)):
     ?>
 
-								                <!-- Mobile -->
-								                <div class="services-list mobile-only">
-								                <?php foreach (array_chunk($services, 2, true) as $services_group): ?>
+									                <!-- Mobile -->
+									                <div class="services-list mobile-only">
+									                <?php foreach (array_chunk($services, 2, true) as $services_group): ?>
 
-								                    <?php foreach ($services_group as $index2 => $service): ?>
-								                        <div class="service-item<?php if ($index2 % 2 == 0): ?> no-border<?php endif;?>">
+									                    <?php foreach ($services_group as $index2 => $service): ?>
+									                        <div class="service-item<?php if ($index2 % 2 == 0): ?> no-border<?php endif;?>">
                             <p class="subtitle"><?php echo $service['service_name']; ?></p>
                         </div>
                     <?php endforeach;?>
@@ -81,10 +135,10 @@ foreach ($services_group as $index2 => $service):
     }
 
     ?>
-								                        <div class="service-item<?php echo $service_item_classes; ?> scroll-anim fade slide-anim" data-animation='{"animation": "slide", "delay": <?php echo 0.75 + $index2 * 0.1; ?>, "group": 1}'>
-								                            <p class="subtitle"><?php echo $service['service_name']; ?></p>
-								                        </div>
-								                    <?php endforeach;?>
+									                        <div class="service-item<?php echo $service_item_classes; ?> scroll-anim fade slide-anim" data-animation='{"animation": "slide", "delay": <?php echo 0.75 + $index2 * 0.1; ?>, "group": 1}'>
+									                            <p class="subtitle"><?php echo $service['service_name']; ?></p>
+									                        </div>
+									                    <?php endforeach;?>
 
                     <div class="services-row">
                     <?php foreach ($services_group as $service): ?>
@@ -177,24 +231,24 @@ foreach ($agency_clients as $index3 => $agency_client):
         $pos = strpos($agency_client['client_logo']['url'], 'wp-content/');
         $client_logo_svg_code = file_get_contents(wp_normalize_path($_SERVER["DOCUMENT_ROOT"] . '/' . substr($agency_client['client_logo']['url'], $pos)));
         ?>
-																                    <li class="agency-client scroll-anim fade scale-anim clip" data-animation='{"animation": "scale", "delay": <?php echo 0.3 + $index3 * 0.06; ?>, "group": 3}'>
-																                        <?php
+																		                    <li class="agency-client scroll-anim fade scale-anim clip" data-animation='{"animation": "scale", "delay": <?php echo 0.3 + $index3 * 0.06; ?>, "group": 3}'>
+																		                        <?php
         if ($client_logo_svg_code !== false) {
             echo $client_logo_svg_code;
         }
         ?>
-																                        <?php
+																		                        <?php
         if (!empty($agency_client['client_hover_text'])):
         ?>
-																                        <div class="hover-text copy<?php echo $classes; ?>">
-																                            <div class="hover-text-content">
-																                                <?php echo $agency_client['client_hover_text']; ?>
-																                                <span class="client-name"><?php echo $agency_client['client_name']; ?></span>
-																                            </div>
-																                        </div>
-																                        <?php endif;?>
-								                    </li>
-								                <?php
+																		                        <div class="hover-text copy<?php echo $classes; ?>">
+																		                            <div class="hover-text-content">
+																		                                <?php echo $agency_client['client_hover_text']; ?>
+																		                                <span class="client-name"><?php echo $agency_client['client_name']; ?></span>
+																		                            </div>
+																		                        </div>
+																		                        <?php endif;?>
+									                    </li>
+									                <?php
 endif;
 endforeach;
 ?>
@@ -211,59 +265,6 @@ endif;
     <div class="vertical-line scroll-fade"></div>
     -->
 
-    <section class="journal-section">
-        <div class="section">
-            <h3 class="title scroll-anim reveal-anim ui" data-animation='{"animation": "reveal", "group": 4}'>
-                <?php echo wrap_letters(get_field('journal_title')); ?>
-            </h3>
-            <div class="title-line scroll-anim expand-anim fade" data-animation='{"animation": "expand", "delay": 0.5, "group": 4}'></div>
-            <div id="journal-subtitle" class="copy ui scroll-anim fade slide-anim" data-animation='{"animation": "slide", "delay": 0.6, "group": 4}'><?php echo get_field('journal_subtitle'); ?></div>
-
-            <?php
-$args = array(
-    'post_type' => array('post'),
-    'post_status' => array('publish'),
-    'posts_per_page' => '3',
-    'order_by' => 'date',
-    'order' => 'DESC',
-);
-
-// The Query
-$loop = new WP_Query($args);
-?>
-            <div id="posts-list" class="ui white-bg">
-                <?php
-$post_index = 0;
-while ($loop->have_posts()): $loop->the_post();
-
-    $post_id = get_the_ID();
-
-    $categories = get_the_category();
-    $cat_name = $categories[0]->cat_name;
-    $excerpt = get_the_excerpt();
-
-    $has_thumbnail = has_post_thumbnail($post_id);
-    ?>
-								                    <?php
-    set_query_var('post_index', $post_index);
-    set_query_var('post_id', $post_id);
-    set_query_var('has_thumbnail', $has_thumbnail);
-    set_query_var('cat_name', $cat_name);
-    set_query_var('excerpt', $excerpt);
-
-    get_template_part('content', 'homepage-post');
-
-    $post_index++;
-    ?>
-
-								                <?php endwhile;?>
-                <?php wp_reset_postdata();?>
-            </div>
-            <div class="wrapper">
-                <a id="more-articles-link" class="strong scroll-anim fade slide-anim" href="<?php echo get_permalink(get_page_by_path('journal')); ?>" data-animation='{"animation": "slide", "delay": 1.45, "group": 4}'>View more articles.</a>
-            </div>
-        </div>
-    </section>
 
 <!--
     <div class="vertical-line scroll-fade margin"></div>
@@ -458,11 +459,11 @@ foreach ($languages_position as $index => $language_position):
         $x = $language_position['x'];
         $y = $language_position['y'];
         ?>
-																    <div class="letter <?php echo $language_class; ?>" data-ease="<?php echo $ease; ?>" style="top: <?php echo $y; ?>%; left: <?php echo $x; ?>%;">
-																        <?php echo $svg_letter; ?>
-																        <p class="letter-language-name"><?php echo ucfirst($label); ?></p>
-																    </div>
-																    <?php
+																		    <div class="letter <?php echo $language_class; ?>" data-ease="<?php echo $ease; ?>" style="top: <?php echo $y; ?>%; left: <?php echo $x; ?>%;">
+																		        <?php echo $svg_letter; ?>
+																		        <p class="letter-language-name"><?php echo ucfirst($label); ?></p>
+																		    </div>
+																		    <?php
     endif;
 endforeach;
 ?>
